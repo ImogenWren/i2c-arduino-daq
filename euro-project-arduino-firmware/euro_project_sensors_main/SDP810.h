e7fe9d837263fb1dc91c6fd4e59f9fbb60d206bb
ImogenWren <imogen.wren91@gmail.com>
2024-01-22 13:53:42 +0000

    adding SDP810 differential pressure sensor

---
+++ euro-project-arduino-firmware/euro_project_sensors_main/SDP810.h
@@ -0,0 +1,16 @@
+/* # Sensirion SDP810 - Differential Pressure Sensor - i2c
+
+- 5v or 3.3v 
+
+ - Pinout: ** Double Check
+
+ 	- SDA:   Yellow
+	- GND: 	  black
+  - Vdd:    Red
+	- SCL:   green
+
+ */
+
+
+ #include "sdpsensor.h"
+#include <Wire.h>
